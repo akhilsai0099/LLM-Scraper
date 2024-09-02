61a23d59be7d2907c3a56f56843ccb181299e6be
akhilsai0099 <akhilsai0099@gmail.com>
2024-09-03 00:42:30 +0530

    connected to the fastapi backend

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
 import streamlit as st
 from scrape import batch_content, clean_body_content, extract_body_content, scrapeWeb
 from parser import parse_with_llm
-
+import requests
+import json
 st.title("AI web Scraper")
-
-conn = st.connection('postgresql', type='sql')
-query = conn.query('select * from scraping_history')
-
-
-for *idx, url in query.itertuples():
-	st.text(url)
 
 url = st.text_input("Enter a Website URL")
 
-
-
 if st.button("Scrape Site"):
 	with st.spinner("Please Wait while we fetch the page"):
-		html = scrapeWeb(url)
-		body = extract_body_content(html)
-		clean_body = clean_body_content(body)
+		headers = {"Content-Type": "application/json"}
+		data = json.dumps({"url":url})
+		res = requests.post('https://llm-scraper.onrender.com/scrape',data=data,headers=headers)
+		
 
-	st.session_state.body_content = clean_body
+	if res.status_code == 200:
+		st.session_state.body_content = res.json()['page_data']
 
 	with st.expander("View Page Content"):
 		st.text_area("Page Content", st.session_state.body_content, height=300)
